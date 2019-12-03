2322add26b8cefc123aaec967774f4eef8accf64
Arnaud Drain <drain.arnaud@hotmail.fr>
2019-12-03 09:26:54 +0100

    Start day 3

--- filereader.js
+++ filereader.js
@@ -1,3 +1,3 @@
-exports.readFile = (separator = '\n') => {
-  return require('fs').readFileSync('input', 'utf-8').split(separator).filter(Boolean).map(n => parseInt(n));
+exports.readFile = (separator = '\n', toNumber = true) => {
+  return require('fs').readFileSync('input', 'utf-8').split(separator).filter(Boolean).map(n => toNumber ? parseInt(n) : n);
 }
