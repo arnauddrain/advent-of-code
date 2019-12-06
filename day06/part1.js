632c2af218b594c2cd080694a2c853525836da3d
Arnaud Drain <drain.arnaud@hotmail.fr>
2019-12-06 12:43:56 +0100

    Optimize day 6

--- day06/part1.js
+++ day06/part1.js
@@ -1,33 +1,21 @@
-// I'm so sorry, I should have use chained lists...
-
 const orbits = require('../filereader.js').readFile('\n', false);
 
-let count = 0;
 const planets = {};
 orbits.forEach((orbit) => {
   const orbitPlanets = orbit.split(')');
-  if (!planets[orbitPlanets[0]]) {
-    planets[orbitPlanets[0]] = [];
-  }
-  if (!planets[orbitPlanets[1]]) {
-    planets[orbitPlanets[1]] = [];
-  }
-  planets[orbitPlanets[0]].push(planets[orbitPlanets[1]]);
+  planets[orbitPlanets[1]] = orbitPlanets[0];
 });
 
-function findOrbits(seekedPlanet, planets) {
-  for (let key in planets) {
-    if (!planets.hasOwnProperty(key)) continue;
-    const planet = planets[key];
-    if (planet.indexOf(seekedPlanet) !== -1) {
-      return 1 + findOrbits(planet, planets);
-    }
+function findPath(seekedPlanet, planets) {
+  if (planets[seekedPlanet]) {
+    return [seekedPlanet].concat(findPath(planets[seekedPlanet], planets));
   }
-  return 0;
+  return [];
 }
 
+let count = 0;
 for (let key in planets) {
   if (!planets.hasOwnProperty(key)) continue;
-  count += findOrbits(planets[key], planets);
+  count += findPath(key, planets).length;
 }
 console.log(count);
--- day06/part2.js
+++ day06/part2.js
@@ -1,42 +1,24 @@
-// I'm so sorry, I should have use chained lists...
-
 const orbits = require('../filereader.js').readFile('\n', false);
 
-let count = 0;
 const planets = {};
 orbits.forEach((orbit) => {
   const orbitPlanets = orbit.split(')');
-  if (!planets[orbitPlanets[0]]) {
-    planets[orbitPlanets[0]] = [];
-  }
-  if (!planets[orbitPlanets[1]]) {
-    planets[orbitPlanets[1]] = [];
-  }
-  planets[orbitPlanets[0]].push(planets[orbitPlanets[1]]);
+  planets[orbitPlanets[1]] = orbitPlanets[0];
 });
 
-function findWay(seekedPlanet, planets, list) {
-  for (let key in planets) {
-    if (!planets.hasOwnProperty(key)) continue;
-    const planet = planets[key];
-    if (planet.indexOf(seekedPlanet) !== -1) {
-      console.log(key);
-      list.unshift(planet)
-      findWay(planet, planets, list);
-    }
+function findPath(seekedPlanet, planets) {
+  if (planets[seekedPlanet]) {
+    return findPath(planets[seekedPlanet], planets).concat([seekedPlanet]);
   }
-  return;
+  return [];
 }
 
-let youList = [];
-findWay(planets['YOU'], planets, youList);
-let santaList = [];
-findWay(planets['SAN'], planets, santaList);
+let youList = findPath('YOU', planets);
+let santaList = findPath('SAN', planets);
 
 for (var i = 0; i < youList.length; i++) {
   if (santaList[i] !== youList[i]) {
-    console.log(santaList.length, youList.length, i);
-    console.log(santaList.length - i + (youList.length - i));
+    console.log(santaList.length - i + (youList.length - i) - 2);
     return;
   }
 }
