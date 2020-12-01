7577dee7db15070a4df17a87410ffb3ba806c402
Arnaud Drain <drain.arnaud@hotmail.fr>
2020-12-01 09:49:24 +0100

    day01

---
+++ day01/part1.ts
@@ -0,0 +1,11 @@
+const inputs = (await Deno.readTextFile("./input")).split("\n").map((input) =>
+  Number(input)
+);
+
+for (let index1 = 0; index1 < inputs.length; index1++) {
+  for (let index2 = index1 + 1; index2 < inputs.length; index2++) {
+    if (inputs[index1] + inputs[index2] === 2020) {
+      console.log(inputs[index1] * inputs[index2]);
+    }
+  }
+}
