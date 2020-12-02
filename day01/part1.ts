a69133f348eacc7946afbaf9204ed8950a6901dd
Arnaud Drain <drain.arnaud@hotmail.fr>
2020-12-02 10:01:10 +0100

    Add prettier

--- day01/part1.ts
+++ day01/part1.ts
@@ -1,8 +1,8 @@
 export {};
 
-const inputs = (await Deno.readTextFile("./input")).split("\n").map((input) =>
-  Number(input)
-);
+const inputs = (await Deno.readTextFile('./input'))
+  .split('\n')
+  .map((input) => Number(input));
 
 for (let index1 = 0; index1 < inputs.length; index1++) {
   for (let index2 = index1 + 1; index2 < inputs.length; index2++) {
--- day02/part2.ts
+++ day02/part2.ts
@@ -1,24 +1,25 @@
 export {};
 
-const inputs = (await Deno.readTextFile("./input")).split("\n").filter(
-  (input) => input.length,
-).map(
-  (input) => {
-    const parts = input.split(" ");
-    const range = parts[0].split("-");
+const inputs = (await Deno.readTextFile('./input'))
+  .split('\n')
+  .filter((input) => input.length)
+  .map((input) => {
+    const parts = input.split(' ');
+    const range = parts[0].split('-');
     return {
       first: Number(range[0]) - 1,
       second: Number(range[1]) - 1,
       letter: parts[1].substr(0, 1),
       password: parts[2],
     };
-  },
-);
+  });
 
 console.log(
   inputs.filter((input) => {
-    return (input.password.charAt(input.first) === input.letter) !==
-      (input.password.charAt(input.second) === input.letter);
+    return (
+      (input.password.charAt(input.first) === input.letter) !==
+      (input.password.charAt(input.second) === input.letter)
+    );
   }).length,
-  "valid passwords",
+  'valid passwords'
 );
