33e2bd5070f57fc0f342df3a57d403bf10cd908a
Arnaud Drain <drain.arnaud@hotmail.fr>
2020-12-02 09:44:57 +0100

    Day 02

---
+++ day02/part2.ts
@@ -0,0 +1,22 @@
+const inputs = (await Deno.readTextFile("./input")).split("\n").filter(
+  (input) => input.length,
+).map(
+  (input) => {
+    const parts = input.split(" ");
+    const range = parts[0].split("-");
+    return {
+      first: Number(range[0]) - 1,
+      second: Number(range[1]) - 1,
+      letter: parts[1].substr(0, 1),
+      password: parts[2],
+    };
+  },
+);
+
+console.log(
+  inputs.filter((input) => {
+    return (input.password.charAt(input.first) === input.letter) !==
+      (input.password.charAt(input.second) === input.letter);
+  }).length,
+  "valid passwords",
+);
