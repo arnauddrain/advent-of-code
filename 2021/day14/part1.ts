e73fbba228640fb1a60aff3d2f0dba570ddf3000
Arnaud Drain <drain.arnaud@hotmail.fr>
2021-12-15 08:54:49 +0100

    start day14

---
+++ 2021/day14/part1.ts
@@ -0,0 +1,44 @@
+export {};
+
+const file = await Deno.readTextFile('./input');
+const parts = file.trim().split('\n\n');
+let str = parts[0];
+const rules = parts[1]
+  .split('\n')
+  .reduce<{ [key: string]: string }>((acc, line) => {
+    const parts = line.split(' -> ');
+    return { ...acc, [parts[0]]: parts[1] };
+  }, {});
+
+for (let i = 0; i < 10; i++) {
+  let tmpStr = str[0];
+  for (let index = 0; index < str.length - 1; index++) {
+    const pair = str.slice(index, index + 2);
+    if (rules[pair]) {
+      tmpStr += rules[pair];
+    }
+    tmpStr += str[index + 1];
+  }
+  str = tmpStr;
+}
+
+const occurrences: { [key: string]: number } = {};
+for (let i = 0; i < str.length; i++) {
+  const char = str[i];
+  if (occurrences[char]) {
+    occurrences[char]++;
+  } else {
+    occurrences[char] = 1;
+  }
+}
+
+const min = Object.entries(occurrences).reduce<[string, number]>(
+  (min, entry) => (min[0] === '' || entry[1] < min[1] ? entry : min),
+  ['', 0]
+);
+const max = Object.entries(occurrences).reduce<[string, number]>(
+  (max, entry) => (max[0] === '' || entry[1] > max[1] ? entry : max),
+  ['', 0]
+);
+
+console.log(max[1] - min[1]);
